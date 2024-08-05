e1efd4ec4fd7d796af648c184c6cb2c2a07d1330
Polina Gushcha <polinagushcha@Polinas-MacBook-Pro.local>
2024-08-05 22:00:43 +0300

    init: install ngrx

--- YoutubeClientApp/src/app/app.config.ts
+++ YoutubeClientApp/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { layoutRoutes } from './layout/layout.routers';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter([...appRoutes, ...layoutRoutes]),
-  ],
+    provideStore()
+],
 };
