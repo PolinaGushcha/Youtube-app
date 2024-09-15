d5a7400308281b111e2a26350a5947db8d940f1d
igor2000xp <igorring@gmail.com>
2024-09-15 19:29:37 +0200

    feat: AppComponent and LoginService tests

--- YoutubeClientApp/src/app/app.component.spec.ts
+++ YoutubeClientApp/src/app/app.component.spec.ts
@@ -1,29 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [AppComponent],
+//     }).compileComponents();
+//   });
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have the 'my-app' title`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('my-app');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement as HTMLElement;
+//     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
+//   });
+// });
+
+describe('App Component', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'my-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
+  });
+
+  it('Testing creating component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Testing title', () => {
+    expect(component.title).toEqual('my-app');
   });
 });
--- YoutubeClientApp/src/app/auth/services/login.service.spec.ts
+++ YoutubeClientApp/src/app/auth/services/login.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
-
+import { IAuth } from '../../types/auth';
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
+  const testKey = 'authKey';
+  const testValue: IAuth = { username: 'test', password: '12345' };
 
+  // Mock localStorage.setItem - another variant
+  //   jest.spyOn(localStorage, 'setItem');
+  //   localStorage.setItem = jest.fn();
+  //   // Mock localStorage.getItem
+  //   jest.spyOn(localStorage, 'getItem');
+  //   localStorage.getItem = jest.fn(() => JSON.stringify(testValue));
+  //   // Mock localStorage.removeItem
+  //   jest.spyOn(localStorage, 'removeItem');
+  //   localStorage.removeItem = jest.fn();
+  const localStorageSetItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+  const localStorageGetItemSpy = jest.spyOn(Storage.prototype, 'getItem');
+  const localStorageRemoveItemSpy = jest.spyOn(Storage.prototype, 'removeItem');
+
+  // beforeEach(() => {
+  //   service = new LoginService();
+  // });
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LoginService],
+    });
     service = TestBed.inject(LoginService);
+
+    // const localStorageSetItem = jest.spyOn(Storage.prototype, 'setItem');
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(LoginService).toBeTruthy();
+  });
+
+  it('should call localStorage.setItem with correct arguments', () => {
+    // Use spyOn to mock localStorage.setItem
+    // const localStorageSetItem = jest.spyOn(Storage.prototype, 'setItem');
+
+    service.setObject(testKey, testValue);
+
+    // Check if localStorage.setItem was called with the correct arguments
+    expect(localStorageSetItemSpy).toHaveBeenCalledWith(testKey, JSON.stringify(testValue));
+    // expect(localStorage.setItem).toHaveBeenCalledWith(testKey, JSON.stringify(testValue));
+  });
+
+  it('should read an object from localStorage', () => {
+    // const localStorageGetItem = jest.spyOn(Storage.prototype, 'getItem');
+    const result = service.getObject(testKey);
+    expect(localStorageGetItemSpy).toHaveBeenCalledWith(testKey);
+    // expect(localStorage.getItem).toHaveBeenCalledWith(testKey);
+    expect(result).toEqual({ username: 'test', password: '12345' });
+  });
+
+  it('should be removed from localStorage', () => {
+    // const localStorageRemoveItem = jest.spyOn(Storage.prototype, 'removeItem');
+    service.removeObject(testKey);
+    expect(localStorageRemoveItemSpy).toHaveBeenCalledWith(testKey);
+    // expect(localStorage.removeItem).toHaveBeenCalledWith(testKey);
   });
 });
