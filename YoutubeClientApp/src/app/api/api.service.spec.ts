f0cb7d8416d257121fd13e5e23fc862dedf71a1a
igor2000xp <igorring@gmail.com>
2024-09-18 22:51:52 +0200

    feat: install Jest

--- YoutubeClientApp/src/app/api/api.service.spec.ts
+++ YoutubeClientApp/src/app/api/api.service.spec.ts
@@ -1,13 +1,18 @@
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { ApiService } from './api.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ApiService, provideHttpClient, provideHttpClientTesting],
+    });
     service = TestBed.inject(ApiService);
+    http = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
