a3652658d0ce1a0733cde027b6ca3e4991cd0f9b
igor2000xp <igorring@gmail.com>
2024-09-18 23:46:48 +0200

    feat: add some environment variables

--- YoutubeClientApp/src/app/api/api.service.ts
+++ YoutubeClientApp/src/app/api/api.service.ts
@@ -1,5 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+
+export enum ApiPartEnum {
+  snippet = 'snippet',
+  statistics = 'statistics',
+}
+
+export enum ApiResourceEnum {
+  search = 'search',
+  videos = 'videos',
+}
 
 @Injectable({
   providedIn: 'root',
@@ -7,10 +18,11 @@ import { Injectable } from '@angular/core';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  public apiResource = ['search', 'videos'];
-  public baseApiUrl = 'https://www.googleapis.com/youtube/v3/';
-  public apiPart = ['snippet', 'statistics'];
-  public apiKey = 'AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM';
+  // public apiResource = ['search', 'videos'];
+  public baseApiUrl = environment.BASE_API_URL;
+  // public apiPart = ['snippet', 'statistics'];
+  public apiKey = environment.API_KEY;
+  // public apiKey = 'AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM';
   public apiType = 'video';
   public apiQ = '';
   public apiId = '';
@@ -18,24 +30,24 @@ export class ApiService {
 
   getYoutubeApiVideos(value?: string) {
     const urlParams = new HttpParams()
-      .set('part', this.apiPart[0])
+      .set('part', ApiPartEnum.snippet)
       .set('key', this.apiKey)
       .set('type', this.apiType)
       .set('q', value || this.apiQ)
       .set('maxResults', this.apiMaxResults);
     const options = { params: urlParams };
-    return this.http.get(`${this.baseApiUrl}${this.apiResource[0]}`, options);
+    return this.http.get(`${this.baseApiUrl}${ApiResourceEnum.search}`, options);
   }
 
   getVideoStatistic(value: string) {
-    const urlParams = new HttpParams().set('part', this.apiPart[1]).set('key', this.apiKey).set('id', value);
+    const urlParams = new HttpParams().set('part', ApiPartEnum.statistics).set('key', this.apiKey).set('id', value);
     const options = { params: urlParams };
-    return this.http.get(`${this.baseApiUrl}${this.apiResource[1]}`, options);
+    return this.http.get(`${this.baseApiUrl}${ApiResourceEnum.videos}`, options);
   }
 
   getYoutubeApiItem(id: string) {
     const urlParams = new HttpParams().set('part', 'snippet').set('key', this.apiKey).set('id', id);
     const options = { params: urlParams };
-    return this.http.get(`${this.baseApiUrl}${this.apiResource[1]}`, options);
+    return this.http.get(`${this.baseApiUrl}${ApiResourceEnum.videos}`, options);
   }
 }
--- YoutubeClientApp/src/environments/environment.ts
+++ YoutubeClientApp/src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  BASE_API_URL: 'https://www.googleapis.com/youtube/v3/',
+  API_KEY: 'AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM',
+};
