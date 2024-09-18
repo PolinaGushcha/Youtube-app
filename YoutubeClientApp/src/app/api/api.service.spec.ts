ba7ca5e953a32bdd58e50efb41bcf5c329a2c57b
igor2000xp <igorring@gmail.com>
2024-09-19 00:15:01 +0200

    feat: getVideoStatistic http request test

--- YoutubeClientApp/src/app/api/api.service.spec.ts
+++ YoutubeClientApp/src/app/api/api.service.spec.ts
@@ -25,18 +25,33 @@ describe('HTTPVideosService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('tests http request', () => {
-    it('should works getYoutubeApiVideos(angular)', async () => {
+  describe('tests http getYoutubeApiVideos(angular) request', () => {
+    it('shuld have correct params', async () => {
       const searchString = 'angular';
       const responseExample = [{ id: '1', name: 'foo' }];
       const url =
         'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM&type=video&q=angular&maxResults=30';
       const vedeo$ = service.getYoutubeApiVideos(searchString);
       const videoPromise = firstValueFrom(vedeo$);
-      const req = http.expectOne(url, 'Http mock');
+      const req = http.expectOne(url, 'Http video mock');
       expect(req.request.method).toEqual('GET');
       req.flush(responseExample);
       expect(await videoPromise).toEqual(responseExample);
+    });
+  });
+
+  describe('tests http getVideoStatistic(id) request', () => {
+    it('shuld have correct params', async () => {
+      const id = 'ID_string';
+      const requestStat =
+        'https://www.googleapis.com/youtube/v3/videos?part=statistics&key=AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM&id=ID_string';
+      const responseExample = [{ id: '1', statistic: 'foo' }];
+      const stat$ = service.getVideoStatistic(id);
+      const statePromise = firstValueFrom(stat$);
+      const req = http.expectOne(requestStat, 'Http stat mock');
+      expect(req.request.method).toEqual('GET');
+      req.flush(responseExample);
+      expect(await statePromise).toEqual(responseExample);
     });
   });
 });
