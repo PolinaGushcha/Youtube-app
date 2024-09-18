20f02f45c9767d2fa564232074f328e5052c21f1
igor2000xp <igorring@gmail.com>
2024-09-18 23:28:09 +0200

    feat: first http test with provideHttpClientTesting

--- YoutubeClientApp/src/app/api/api.service.spec.ts
+++ YoutubeClientApp/src/app/api/api.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule, HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
@@ -10,11 +10,15 @@ describe('HTTPVideosService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      // providers: [ApiService, provideHttpClient(), provideHttpClientTesting()],
+      // imports: [HttpClientTestingModule],
+      providers: [ApiService, provideHttpClient(), provideHttpClientTesting()],
     });
     service = TestBed.inject(ApiService);
     http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
@@ -22,13 +26,17 @@ describe('HTTPVideosService', () => {
   });
 
   describe('tests http request', () => {
-    it('should works getYoutubeApiVideos(angular)', () => {
+    it('should works getYoutubeApiVideos(angular)', async () => {
       const searchString = 'angular';
+      const responseExample = [{ id: '1', name: 'foo' }];
       const url =
         'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM&type=video&q=angular&maxResults=30';
       const vedeo$ = service.getYoutubeApiVideos(searchString);
       const videoPromise = firstValueFrom(vedeo$);
       const req = http.expectOne(url, 'Http mock');
+      expect(req.request.method).toEqual('GET');
+      req.flush(responseExample);
+      expect(await videoPromise).toEqual(responseExample);
     });
   });
 });
