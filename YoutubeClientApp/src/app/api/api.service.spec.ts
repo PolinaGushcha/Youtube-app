e8a65a91e5b32faaf92e7c4f1e139ff6ea5c2cb1
igor2000xp <igorring@gmail.com>
2024-09-18 23:18:13 +0200

    feat: working http address

--- YoutubeClientApp/src/app/api/api.service.spec.ts
+++ YoutubeClientApp/src/app/api/api.service.spec.ts
@@ -1,7 +1,8 @@
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom';
 
 describe('HTTPVideosService', () => {
   let service: ApiService;
@@ -9,8 +10,8 @@ describe('HTTPVideosService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [],
-      providers: [ApiService, provideHttpClient(), provideHttpClientTesting()],
+      imports: [HttpClientTestingModule],
+      // providers: [ApiService, provideHttpClient(), provideHttpClientTesting()],
     });
     service = TestBed.inject(ApiService);
     http = TestBed.inject(HttpTestingController);
@@ -18,5 +19,16 @@ describe('HTTPVideosService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('tests http request', () => {
+    it('should works getYoutubeApiVideos(angular)', () => {
+      const searchString = 'angular';
+      const url =
+        'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyB-sYrDcNSM42Dhm8HPyPt5qHpjmG9dkbM&type=video&q=angular&maxResults=30';
+      const vedeo$ = service.getYoutubeApiVideos(searchString);
+      const videoPromise = firstValueFrom(vedeo$);
+      const req = http.expectOne(url, 'Http mock');
+    });
   });
 });
