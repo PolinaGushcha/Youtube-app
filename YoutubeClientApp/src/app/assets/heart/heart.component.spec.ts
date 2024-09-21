a3c02f11df02834e47ff24219b970b3d18fa172d
igor2000xp <igorring@gmail.com>
2024-09-21 11:46:37 +0200

    feat: implement base of component test to check if the component was created

--- YoutubeClientApp/src/app/assets/heart/heart.component.spec.ts
+++ YoutubeClientApp/src/app/assets/heart/heart.component.spec.ts
@@ -1,23 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeartComponent } from './heart.component';
 
-describe('HeartComponent', () => {
+describe('HeartComponent test', () => {
   let component: HeartComponent;
   let fixture: ComponentFixture<HeartComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeartComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HeartComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
