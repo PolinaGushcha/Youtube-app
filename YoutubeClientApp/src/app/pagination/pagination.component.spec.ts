9f61271bcda7930f61e5a1eb42d31d92d87ef845
igor2000xp <igorring@gmail.com>
2024-09-21 21:00:19 +0200

    feat: implement a pagination component test (render list) and light correct of the pagination

--- YoutubeClientApp/src/app/pagination/pagination.component.spec.ts
+++ YoutubeClientApp/src/app/pagination/pagination.component.spec.ts
@@ -1,23 +1,37 @@
+// https://www.notion.so/Transcription-16-Testing-inputs-and-outputs-in-Angular-components-1036bddbc491808b8ab1c4506f69ff0e
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PaginationComponent } from './pagination.component';
+import { UtilsService } from './utils.service';
+import { By } from '@angular/platform-browser';
 
-describe('PaginationComponent', () => {
+describe('PaginationComponent test', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PaginationComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PaginationComponent],
+      providers: [UtilsService],
+    }).compileComponents();
 
+    // const utilsService = Inject(UtilsService);
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
+    component.totalItems = 50;
+    component.itemsPerPage = 10;
+    component.currentPage = 1;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render pagination list', () => {
+    const container = fixture.debugElement.queryAll(By.css('[data-testid="page-container"]'));
+
+    expect(container.length).toBe(5);
+    expect(container[0].nativeElement.textContent).toContain('1');
   });
 });
--- YoutubeClientApp/src/app/pagination/pagination.component.ts
+++ YoutubeClientApp/src/app/pagination/pagination.component.ts
@@ -1,20 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { UtilsService } from './utils.service';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers: [UtilsService],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss',
 })
-export class PaginationComponent {
-  @Input() totalItems!: number;
+export class PaginationComponent implements OnInit {
+  @Input() totalItems = 40;
   @Input() itemsPerPage = 20;
   @Input() currentPage = 1;
+  pagesCount = 1;
+  pages: number[] = [];
 
   @Output() currentPageChange = new EventEmitter<number>();
-  @Output() pageChanged = new EventEmitter<number>();
+  utilsService = inject(UtilsService);
 
+  ngOnInit(): void {
+    this.pagesCount = Math.ceil(this.totalItems / this.itemsPerPage);
+    this.pages = this.pagesCount > 0 ? this.utilsService.range(1, this.pagesCount + 1) : [];
+  }
+
+  selectPage(page = 1): void {
+    this.currentPageChange.emit(page);
+  }
+  /*
+   ******************************
+   */
   get totalPages(): number {
     return Math.ceil(this.totalItems / this.itemsPerPage);
   }
@@ -23,7 +39,6 @@ export class PaginationComponent {
     if (page >= 1 && page <= this.totalPages) {
       this.currentPage = page;
       this.currentPageChange.emit(this.currentPage);
-      this.pageChanged.emit(this.currentPage);
     }
   }
 
